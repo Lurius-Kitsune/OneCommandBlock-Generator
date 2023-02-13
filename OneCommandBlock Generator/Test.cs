89783c8b390370c349b8c5b0153dbc625e3aeb2d
Lurius-Kitsune <LucasFox@outlook.fr>
2023-02-13 01:35:07 +0100

    [New] Teste Methode for  Init

---
+++ OneCommandBlock Generator/Test.cs
@@ -0,0 +1,27 @@
+﻿using OneCommandBlock_Generator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outils
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            TesteBuildInitOnly();
+            Console.ReadKey();
+        }
+
+        public static void TesteBuildInitOnly()
+        {
+            Generator monOneCommand = new Generator("Mods de Lurius", "Voici un No mod");
+
+            List<string> initCommand = new List<string>();
+            initCommand.Add("/say hello");
+            Console.WriteLine($"[Info]{monOneCommand.OneCommandBuild(initCommand)}");
+        }
+    }
+}
