fd41a60f296d88d617e8e0a3a8b55043e7a8c9a9
Lurius-Kitsune <LucasFox@outlook.fr>
2023-02-18 17:09:21 +0100

    [Edit] Surcharge of AddCommand

--- OneCommandBlock Generator/Application.cs
+++ OneCommandBlock Generator/Application.cs
@@ -14,6 +14,7 @@ namespace OneCommandBlock_Generator
         {
             string answer = "0";
             List<string> loopList = new List<string>();
+            List<string> initList = new List<string>();
             Application app = new Application();
 
             Introduction(app.v, app.vType);
@@ -23,7 +24,6 @@ namespace OneCommandBlock_Generator
             while (answer != "1" || answer != "2")
                 if (answer == "1")
                 {
-                    List<string> initList = new List<string>();
                     AddCommand(ref initList);
                 }
                 else if (answer != "2")
@@ -33,6 +33,8 @@ namespace OneCommandBlock_Generator
                 }
             Console.WriteLine("Add you're loop command for the no-mod :");
             AddCommand(ref loopList);
+            OneCommandGenerator oneCommand = new OneCommandGenerator("Lurius", "Test", 5, 5);
+            oneCommand.OneCommandBuild(loopList, initList)
         }
 
         private static void Introduction(double v, string vType)
@@ -59,6 +61,64 @@ namespace OneCommandBlock_Generator
                     if (command is not null)
                     {
                         listX.Add(command);
+                        i++;
+                        Console.WriteLine("Continue? ? [Yes] or [No]");
+                        answer = Console.ReadLine();
+                        do
+                        {
+                            if (answer is not null)
+                            {
+                                if (answer.ToLower() == "no")
+                                {
+                                    stop = true;
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("[Warning] Please choose an option");
+                            }
+                        } while (answer is null);
+                    }
+                    else
+                    {
+                        Console.WriteLine("[Warning] Please choose an option");
+                    }
+                } while (command is null);
+            }
+        }
+
+        private static void AddCommand(ref Dictionary<string,bool> listX)
+        {
+            bool stop = false, cond = false;
+            string command = "", answer = "";
+            int i = 1;
+            while (!stop)
+            {
+                Console.WriteLine($"Command n°{i} : ");
+                command = Console.ReadLine();
+                do
+                {
+                    if (command is not null)
+                    {
+                        Console.WriteLine("Conditionnal ?: true or false");
+                        do
+                        {
+                            if (answer is not null)
+                            {
+                                if (answer.ToLower() == "true")
+                                {
+                                    cond = true;
+                                }
+                                else if (answer.ToLower() == "false"){
+                                    cond = false;
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("[Warning] Please choose an option");
+                            }
+                        } while (answer is null);
+                        listX.Add(command, cond);
                         i++;
                         Console.WriteLine("Continue? ? [Yes] or [No]");
                         answer = Console.ReadLine();
